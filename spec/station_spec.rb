7029f184b10eb7fe4cbbe5844ba362b009f66777
dpwdec <declankolakowski@gmail.com>
2020-04-22 16:31:34 +0100

    Added name and zone variables with types to the station class.

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,4 +1,19 @@
 require 'station'
 
 describe Station do
+
+  let(:subject) { described_class.new("Paddington", 1) }
+
+  describe "#name" do
+    it "is a String" do
+      expect(subject.name).to be_a_kind_of(String)
+    end
+  end
+
+  describe "#zone" do
+    it "is a Integer" do
+      expect(subject.zone).to be_a_kind_of(Integer)
+    end
+  end
+
 end
