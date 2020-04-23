47869e0f3284efb0a9a82937c229cbbc55254c4e
dpwdec <declankolakowski@gmail.com>
2020-04-23 20:13:43 +0100

    added entry and exit stations:

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,5 +1,14 @@
 require 'journey'
 
 describe Journey do
-  
+
+  it { is_expected.to respond_to(:entry_station) }
+  it { is_expected.to respond_to(:exit_station) }
+
+  describe "#complete?" do
+    it "returns false if exit_station or entry_station are nil" do
+
+    end
+  end
+
 end
